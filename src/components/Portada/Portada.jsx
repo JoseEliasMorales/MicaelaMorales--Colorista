589a16521344c81bcc5e26c626f8724380bd64ca
Joselias <joselias.dev@gmail.com>
2023-05-18 15:31:09 -0300

    correcion nombre imagen

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -1,5 +1,5 @@
 import { Box, Container } from '@mui/material'
-import imagenPortada from './imagenPortada.jpg'
+import imagenPortada from './ImagenPortada.jpg'
 import './portada.css'
 
 const Portada = () => {
